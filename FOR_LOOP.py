d709b5d50b9eaa8c3a698f6c8fd3b4f0073592a2
Anshuman Pandey <anshumanpandeyx100@gmail.com>
2021-08-10 17:17:25 +0530

    updated

--- FOR_LOOP.py
+++ FOR_LOOP.py
@@ -1,4 +1,4 @@
-def grt_3(a = int(input('enter a')) , b = int(input('enter b')) , c = int(input('enter c'))):
+def grt_3(a,b,c):
     if a > b and a > c:
         print(a)
     elif b > a and b>c:
@@ -8,5 +8,3 @@ def grt_3(a = int(input('enter a')) , b = int(input('enter b')) , c = int(input(
     else:
         print(a,b,c)
 
-
-grt_3(3,4,5)
---
+++ module.py
@@ -0,0 +1,2 @@
+import FOR_LOOP as FL
+FL.grt_3(3, 6, 9)
